6262311d00ae0c9a9ecaccb02117fda042b918f4
JoaoB <joao.marinho@outlook.com>
2016-02-19 11:52:42 -0200

    DeviseTokeAuth added

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,13 @@
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  include DeviseTokenAuth::Concerns::User
+
   validates :type, inclusion: {:in => %w(Client)}
+  validates :first_name, presence: true
+  validates :last_name, presence: true
 
   def name
     first_name + " " + last_name
