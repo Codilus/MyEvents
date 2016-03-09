287b456dc0b275fc903b1e6637b31fc6d3a40453
Jun <felipe.jun.ohira@gmail.com>
2016-03-08 22:56:34 -0300

    Fixing default value for offers factory

--- spec/factories/offers.rb
+++ spec/factories/offers.rb
@@ -4,9 +4,22 @@ FactoryGirl.define do
     promoter
     budget_price 123.45
     budget_description "This is the budget description"
+    status "PENDING_BUDGET"
 
-    factory :offer_pending_budget do
-      status "PENDING_BUDGET"
+    factory :offer_pending_confirmation do
+      status "PENDING_CONFIRMATION"
+    end
+
+    factory :offer_refused do
+      status "REFUSED"
+    end
+
+    factory :offer_confirmed do
+      status "CONFIRMED"
+    end
+
+    factory :offer_finished do
+      status "FINISHED"
     end
   end
 end
