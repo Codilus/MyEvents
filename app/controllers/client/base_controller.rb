085e75778f92c74a9d8378a714fcf1db1bc529b2
JoaoB <joao.marinho@outlook.com>
2016-03-11 05:45:42 -0300

    Fixed client base controller

--- app/controllers/client/base_controller.rb
+++ app/controllers/client/base_controller.rb
@@ -1,4 +1,13 @@
 class Client::BaseController < ApplicationController
   include DeviseTokenAuth::Concerns::SetUserByToken
-  # before_action :authenticate_client!
+  before_action :authenticate_client!
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  private
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:last_name, :first_name, :type) }
+    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:last_name, :first_name) }
+  end
+
 end
